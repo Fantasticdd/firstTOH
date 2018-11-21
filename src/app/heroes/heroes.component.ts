66199c1b35351a4c2dcb0aae1b330957bafdcb9d
MUYI97 <yangmuyi97@gmail.com>
2018-11-22 01:24:03 +0800

    2018-11-22 1:23

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,12 +12,14 @@ export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
   selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 }
 
